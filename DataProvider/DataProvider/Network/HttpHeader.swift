479750454d484655806014484a39f8fefe0b7a20
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2023-01-14 15:54:08 +0300

    add network layer

---
+++ DataProvider/DataProvider/Network/HttpHeader.swift
@@ -0,0 +1,31 @@
+//
+//  HTTPHeader.swift
+//  DataProvider
+//
+//  Created by Berkay on 14.01.2023.
+//
+
+import Foundation
+
+public enum HttpHeader {
+    case contentType(value : String? = "application/json")
+    case bearerToken(token : String)
+    
+    var key : String {
+        switch self {
+        case .contentType:
+            return "Content-Type"
+        case .bearerToken(_):
+            return "Authorization"
+        }
+    }
+    
+    var value : String {
+        switch self {
+        case .contentType(let value):
+            return value ?? "application/json"
+        case .bearerToken(let token):
+            return "Bearer \(token)"
+        }
+    }
+}
