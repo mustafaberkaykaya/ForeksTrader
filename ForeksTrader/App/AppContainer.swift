8c6f5d7a794ee92c46a3643655f92545951703f2
Berkay <berkaykaya_2000@hotmail.comgit config --global user.name mustafaberkaykaya>
2023-01-14 18:07:00 +0300

    add service to appcontainer and configure app path

--- ForeksTrader/App/AppContainer.swift
+++ ForeksTrader/App/AppContainer.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import DataProvider
 
 let app = AppContainer()
 
 final class AppContainer {
 
     let router = AppRouter()
+    let foreksService: ForeksServiceProtocol = ForeksService()
 }
